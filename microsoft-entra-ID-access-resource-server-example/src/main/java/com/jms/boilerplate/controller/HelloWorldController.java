d0d79add87f291df4747e752403c6912271186bd
John Melvin Susai <johnmelvin.susai@gmail.com>
2024-03-26 09:51:53 -0400

    Refactoring: added Service in microsoft-entra-ID-access-resource-server-example

--- microsoft-entra-ID-access-resource-server-example/src/main/java/com/jms/boilerplate/controller/HelloWorldController.java
+++ microsoft-entra-ID-access-resource-server-example/src/main/java/com/jms/boilerplate/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.jms.boilerplate.controller;
 
+import com.jms.boilerplate.service.HelloWorldService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,24 +11,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HelloWorldController {
 
+    private final HelloWorldService helloWorldService;
+
+    public HelloWorldController(HelloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
+
     @GetMapping("/helloworld/no-auth")
     @ResponseBody
     public String getHelloWorld() {
-        return "Response from api: Hello World Unrestricted";
+        return helloWorldService.getHelloWorld();
     }
 
     @GetMapping("/helloworld/restricted-auth")
     @ResponseBody
     @PreAuthorize("hasAuthority('SCOPE_Api.ExampleScope1')")
     public String getHelloWorldRestrictedAuthority() {
-        return "Response from api: Hello World Restricted Authority";
+        return helloWorldService.getHelloWorldRestrictedAuthority();
     }
 
     @GetMapping("/helloworld/restricted-any-auth")
     @ResponseBody
     @PreAuthorize("hasAnyAuthority('SCOPE_Api.ExampleScope2','SCOPE_Api.ExampleScope3')")
     public String getHelloWorldRestrictedAnyAuthority() {
-        return "Response from api: Hello World Restricted Any Authority";
+        return helloWorldService.getHelloWorldRestrictedAnyAuthority();
     }
 
 }
